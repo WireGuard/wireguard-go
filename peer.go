e21ea58db36b8caf3b7da075c9c39505449d0110
Jason A. Donenfeld <Jason@zx2c4.com>
2018-05-13 23:27:28 +0200

    Prettier abbreviation

--- peer.go
+++ peer.go
@@ -147,7 +147,7 @@ func (peer *Peer) String() string {
 	base64Key := base64.StdEncoding.EncodeToString(peer.handshake.remoteStatic[:])
 	abbreviatedKey := "invalid"
 	if len(base64Key) == 44 {
-		abbreviatedKey = base64Key[0:4] + "..." + base64Key[40:44]
+		abbreviatedKey = base64Key[0:4] + "…" + base64Key[39:43]
 	}
 	return fmt.Sprintf("peer(%s)", abbreviatedKey)
 }
