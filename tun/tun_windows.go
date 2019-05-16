7d5f5bcc0dedc8272c8c3cdbc345876f24c4ecfe
Simon Rozman <simon@rozman.si>
2019-05-16 10:33:47 +0200

    wintun: change acronyms to uppercase
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- tun/tun_windows.go
+++ tun/tun_windows.go
@@ -48,7 +48,7 @@ type NativeTun struct {
 	wrBuff       *exchgBufWrite
 	events       chan TUNEvent
 	errors       chan error
-	forcedMtu    int
+	forcedMTU    int
 }
 
 func packetAlign(size uint32) uint32 {
@@ -97,7 +97,7 @@ func CreateTUN(ifname string) (TUNDevice, error) {
 		wrBuff:    &exchgBufWrite{},
 		events:    make(chan TUNEvent, 10),
 		errors:    make(chan error, 1),
-		forcedMtu: 1500,
+		forcedMTU: 1500,
 	}, nil
 }
 
@@ -221,12 +221,12 @@ func (tun *NativeTun) Close() error {
 }
 
 func (tun *NativeTun) MTU() (int, error) {
-	return tun.forcedMtu, nil
+	return tun.forcedMTU, nil
 }
 
 //TODO: This is a temporary hack. We really need to be monitoring the interface in real time and adapting to MTU changes.
-func (tun *NativeTun) ForceMtu(mtu int) {
-	tun.forcedMtu = mtu
+func (tun *NativeTun) ForceMTU(mtu int) {
+	tun.forcedMTU = mtu
 }
 
 func (tun *NativeTun) Read(buff []byte, offset int) (int, error) {
