a12b76578443b8f3f237d1a3730d829c9906a010
Simon Rozman <simon@rozman.si>
2019-08-30 14:41:33 +0200

    namespaceapi: initial version
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ tun/wintun/namespaceapi/mksyscall.go
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2019 WireGuard LLC. All Rights Reserved.
+ */
+
+package namespaceapi
+
+//go:generate go run $GOROOT/src/syscall/mksyscall_windows.go -output zsyscall_windows.go namespaceapi_windows.go
