f8fdbe76788b090c939e71a8a649744ee1bd322b
David Crawshaw <crawshaw@tailscale.com>
2020-02-19 10:09:24 -0500

    rwcancel: no-op builds for windows and darwin
    This lets us include the package on those platforms in a
    followup commit where we split out a conn package from device.
    It also lets us run `go test ./...` when developing on macOS.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- rwcancel/select_default.go
+++ rwcancel/select_default.go
@@ -1,4 +1,4 @@
-// +build !linux
+// +build !linux,!windows
 
 /* SPDX-License-Identifier: MIT
  *
@@ -10,5 +10,6 @@ package rwcancel
 import "golang.org/x/sys/unix"
 
 func unixSelect(nfd int, r *unix.FdSet, w *unix.FdSet, e *unix.FdSet, timeout *unix.Timeval) error {
-	return unix.Select(nfd, r, w, e, timeout)
+	_, err := unix.Select(nfd, r, w, e, timeout)
+	return err
 }
