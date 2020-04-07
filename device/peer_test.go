7a5c5896e773088e96a446d359037b247c81f025
David Crawshaw <crawshaw@tailscale.com>
2020-04-07 14:52:17 +1000

    device: move stats fields back down and add test diagnostics
    This reverts the movement of fields from d49f4e9.
    That commit was cherry-picked from another branch where a field
    had changed and misaligned the atomic fields. After cherry-picking,
    moving the fields was no longer necessary but got dragged along.
    
    Signed-off-by: David Crawshaw <crawshaw@tailscale.com>

--- device/peer_test.go
+++ device/peer_test.go
@@ -6,6 +6,7 @@
 package device
 
 import (
+	"reflect"
 	"testing"
 	"unsafe"
 )
@@ -24,6 +25,19 @@ func checkAlignment(t *testing.T, name string, offset uintptr) {
 // hard segfault at runtime.
 func TestPeerAlignment(t *testing.T) {
 	var p Peer
+
+	typ := reflect.TypeOf(p)
+	t.Logf("Peer type size: %d, with fields:", typ.Size())
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+		t.Logf("\t%30s\toffset=%3v\t(type size=%3d, align=%d)",
+			field.Name,
+			field.Offset,
+			field.Type.Size(),
+			field.Type.Align(),
+		)
+	}
+
 	checkAlignment(t, "Peer.stats", unsafe.Offsetof(p.stats))
 	checkAlignment(t, "Peer.isRunning", unsafe.Offsetof(p.isRunning))
 }
