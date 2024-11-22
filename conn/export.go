9c57ff270274ce4d9e01d8ffb34a4e0c8b775e1d
Zoltan Papp <zoltan.pmail@gmail.com>
2024-11-22 17:43:26 +0100

    Export list of control functions

--- conn/export.go
+++ conn/export.go
@@ -9,4 +9,8 @@ var (
 	GetSrcFromControl      = getSrcFromControl
 
 	GetGSOSize = getGSOSize
+
+	// export controlFns for Android to use
+	// is not thread safe and should only be modified during init.
+	ControlFns = &controlFns
 )
