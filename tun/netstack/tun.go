e3c1354d27f53462801e1b86b4275699a6f9fdac
Jason A. Donenfeld <Jason@zx2c4.com>
2025-05-04 17:54:57 +0200

    tun/netstack: remove usage of pkt.IsNil()
    Since 3c75945fd ("netstack: remove PacketBuffer.IsNil()") this has been
    invalid. Follow the replacement pattern of that commit.
    
    The old definition inlined to the same code anyway:
    
     func (pk *PacketBuffer) IsNil() bool {
     	return pk == nil
     }
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- tun/netstack/tun.go
+++ tun/netstack/tun.go
@@ -155,7 +155,7 @@ func (tun *netTun) Write(buf [][]byte, offset int) (int, error) {
 
 func (tun *netTun) WriteNotify() {
 	pkt := tun.ep.Read()
-	if pkt.IsNil() {
+	if pkt == nil {
 		return
 	}
 
